f6ab9f9e54c1a62c2b9005c367856e82a777e6dc
Morgan <morganw118@gmail.com>
2020-11-10 17:20:47 -0800

    created cards and call function for forecast information

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
-
+var today = new Date();
+var date = (today.getMonth()+1)+'/'+today.getDate()+'/'+today.getFullYear();
 
 $('#search-btn').on('click', function() {
     var cityName = $('#search-input').val();
     var queryURL = 'https://api.openweathermap.org/data/2.5/weather?q=' + cityName + '&appid=b358465e5aa355de6ef4b2a790684722';
-
+    var forecastURL = 'https://api.openweathermap.org/data/2.5/forecast?q=' + cityName + '&appid=b358465e5aa355de6ef4b2a790684722';
 
     $.ajax({
         url: queryURL,
@@ -12,17 +13,26 @@ $('#search-btn').on('click', function() {
         console.log(response);
 
         var cityDisplayed = response.name;
-        $('#city-display').text(cityDisplayed);
+        $('#city-display').text(cityDisplayed + ' (' + date + ')');
 
         var cityTemp = response.main.temp;
-        $('#weather-details').text('Temperature: ' + cityTemp);
+        $('#temp').text('Temperature: ' + cityTemp);
 
-        var cityHumidity = $('<p>').text(response.main.humidity);
-        console.log(cityHumidity);
-        $('.weather-card').append('Humidity: ' + cityHumidity + '%');
+        var cityHumidity = response.main.humidity;
+        $('#humidity').text('Humidity: ' + cityHumidity + '%');
 
-        var windSpeed = $('<p>').text(response.wind.speed);
-        $('.weather-card').append('Wind Speed: ' + windSpeed + 'MPH');
+        var windSpeed = response.wind.speed;
+        $('#wind-speed').text('Wind Speed: ' + windSpeed + ' MPH');
 
+        var uvIndex = response;
+        $('#uv-index').text('UV Index: ' + uvIndex);
     });
+
+    $.ajax({
+        url: forecastURL,
+        method: 'GET'
+    }).then(function(response) {
+        console.log(response);
+    });
+
 });
